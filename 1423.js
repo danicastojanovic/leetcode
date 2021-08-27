d68dfaff12ec1cb09cae468aec7cc730227f4313
danicastojanovic <dnc.stjnvc@gmail.com>
2021-08-27 14:08:58 -0700

    max points from cards

---
+++ 1423.js
@@ -0,0 +1,67 @@
+/*
+1423. Maximum Points You Can Obtain from Cards
+There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+Your score is the sum of the points of the cards you have taken.
+
+Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+
+
+Example 1:
+
+Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+Output: 12
+Explanation: After the first step, your score will always be 1. However, choosing the rightmost card first will maximize your total score. The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+Example 2:
+
+Input: cardPoints = [2,2,2], k = 2
+Output: 4
+Explanation: Regardless of which two cards you take, your score will always be 4.
+Example 3:
+
+Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+Output: 55
+Explanation: You have to take all the cards. Your score is the sum of points of all cards.
+Example 4:
+
+Input: cardPoints = [1,1000,1], k = 1
+Output: 1
+Explanation: You cannot take the card in the middle. Your best score is 1.
+Example 5:
+
+Input: cardPoints = [1,79,80,1,1,1,200,1], k = 3
+Output: 202
+
+
+Constraints:
+
+1 <= cardPoints.length <= 105
+1 <= cardPoints[i] <= 104
+1 <= k <= cardPoints.length
+*/
+
+var maxScore = function(cardPoints, k) {
+  let n = cardPoints.length;
+  let max = 0;
+  if (!n) return max;
+  if (k <= 0) return max;
+  if (k >= n) return cardPoints.reduce((total, curr) => total + curr, 0);
+  for (let i = 0; i < k; i++) {
+      max += cardPoints[i];
+  }
+  let left = 0;
+  let right = k - 1;
+  let current = max;
+  debugger;
+  while (right >= 0) {
+      current = current + cardPoints[n - 1 + left] - cardPoints[right];
+      max = Math.max(max, current);
+      right -= 1;
+      left -= 1;
+  }
+
+  return max;
+};
